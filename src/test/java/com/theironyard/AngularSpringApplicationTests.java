0f6e0cdb0ad31db3a7dbfdbd6bf2b6e49c919ea1
oakes <zsoakes@gmail.com>
2015-12-10 12:10:24 -0500

    Add test

--- src/test/java/com/theironyard/AngularSpringApplicationTests.java
+++ src/test/java/com/theironyard/AngularSpringApplicationTests.java
@@ -1,18 +1,57 @@
 package com.theironyard;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = AngularSpringApplication.class)
 @WebAppConfiguration
 public class AngularSpringApplicationTests {
 
+    @Autowired
+    UserRepository users;
+
+    @Autowired
+    WebApplicationContext wap;
+
+    MockMvc mockMvc;
+
+    @Before
+    public void before() {
+        users.deleteAll();
+        mockMvc = MockMvcBuilders.webAppContextSetup(wap).build();
+    }
+
 	@Test
-	public void contextLoads() {
+	public void addUser() throws Exception {
+        User user = new User();
+        user.username = "Alice";
+        user.address = "17 Princess St";
+        user.email = "alice@theironyard.com";
+
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(user);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.post("/user")
+                    .content(json)
+                    .contentType("application/json")
+        );
+
+        assertTrue(users.count() == 1);
 	}
 
 }
